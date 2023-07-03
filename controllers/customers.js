573decd1e78d1068703b47c0a55f3ad0728ac581
Charlene A <chichiko90@gmail.com>
2023-07-03 23:31:14 +0800

    Distance calculation function done

--- controllers/customers.js
+++ controllers/customers.js
@@ -45,7 +45,7 @@ const getAllCustomerDirtInfo = (req, res) => {
 }
 
 const getCustomerWeatherDistance = (req, res) => {
-  const query = appointment.find({ email: req.body.email })
+  const query = customers.findOne({ email: req.body.email })
   query.exec(async (err, data) => {
     if (err) {
       console.log('Error: ' + err)
@@ -55,15 +55,27 @@ const getCustomerWeatherDistance = (req, res) => {
     if (data) {
       let weatherDistance = data.weatherDistance
 
-      if (
-        (data.weatherDistance.lat == req.body.distance.lat &&
-        data.weatherDistance.long == req.body.distance.long) || weatherDistance.length < 1
-      ) {
+      // weatherDistance has not been created before
+      if (weatherDistance == undefined || Object.keys(weatherDistance).length < 1) {
+
+        let newWeatherDistance = {
+            old: {
+                lat: req.body.weatherDistance.lat,
+                long: req.body.weatherDistance.long,
+                weather: req.body.weatherDistance.weather,
+            },
+            new: {
+                lat: req.body.weatherDistance.lat,
+                long: req.body.weatherDistance.long,
+                weather: req.body.weatherDistance.weather
+            }
+        }
+
         let weaDis = await customers.updateOne(
           { id: req.body.id },
           {
             $set: {
-              weatherDistance: weatherDistance
+              weatherDistance: newWeatherDistance
             }
           }
         )
@@ -71,21 +83,92 @@ const getCustomerWeatherDistance = (req, res) => {
         if (weaDis) {
           return res.status(200).send({
             message:
-              'New distance: ' +
-              req.body.distance.lat +
-              ', ' +
-              req.body.distance.long +
-              '.'
+              'New distance: ' + req.body.weatherDistance.lat + ', ' + req.body.weatherDistance.long + '.'
           })
         }
       } else {
-        return res.status(200).send({
-            message: "No distance change."
-        })
+        // weather has been created before
+
+        // weather and distance is the same
+        if (data.weatherDistance.new.lat == req.body.weatherDistance.lat && data.weatherDistance.new.long == req.body.weatherDistance.long) {
+
+            return res.status(200).send({
+                message: "No distance change."
+            })
+        }
+
+        // weather and distance has changed from previously, move current lat, long and weather into old and rewrite new
+
+        // but first, calculate the distance travelled
+        const latOld = data.weatherDistance.new.lat;
+        const latNew = req.body.weatherDistance.lat;
+        const longOld = data.weatherDistance.new.long
+        const longNew = req.body.weatherDistance.long
+
+        // use distance() function to calculate distance travelled
+        let distanceTravelled = distance(latOld, latNew, longOld, longNew);
+
+        let newWeatherDistance = {
+            old: {
+                lat: latOld,
+                long: longOld,
+                weather: data.weatherDistance.weather,
+            },
+            new: {
+                lat: latNew,
+                long: longNew,
+                weather: req.body.weatherDistance.weather
+            },
+            distance: distanceTravelled
+        }
+
+        let weaDis = await customers.updateOne(
+            { id: req.body.id },
+            {
+              $set: {
+                weatherDistance: newWeatherDistance
+              }
+            }
+          )
+  
+          if (weaDis) {
+            return res.status(200).send({
+              message:
+                'New distance: ' + req.body.weatherDistance.lat + ', ' + req.body.weatherDistance.long + '.'
+            })
+          }
       }
     }
   })
 }
+
+const distance = (lat1, lat2, lon1, lon2) => {
+  // The math module contains a function
+  // named toRadians which converts from
+  // degrees to radians.
+  lon1 = (lon1 * Math.PI) / 180
+  lon2 = (lon2 * Math.PI) / 180
+  lat1 = (lat1 * Math.PI) / 180
+  lat2 = (lat2 * Math.PI) / 180
+
+  // Haversine formula
+  let dlon = lon2 - lon1
+  let dlat = lat2 - lat1
+  let a =
+    Math.pow(Math.sin(dlat / 2), 2) +
+    Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlon / 2), 2)
+
+  let c = 2 * Math.asin(Math.sqrt(a))
+
+  // Radius of earth in kilometers. Use 3956
+  // for miles
+  let r = 6371
+
+  // calculate the result
+  return c * r
+}
+
+
 
 customerRouter.post('/get', async (req, res) => {
   return getCustomer(req, res)
